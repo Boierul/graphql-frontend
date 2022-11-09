31ed799b055f8ca8eab1601ad3f4e1e8dee80710
Dan <pintea.dan988@gmail.com>
2022-11-09 17:49:19 +0100

    Added some basic functionality && minimal inline-styling.

--- src/components/DisplayData.jsx
+++ src/components/DisplayData.jsx
@@ -33,8 +33,18 @@ const GET_MOVIE_BY_NAME = gql`
     }
 `
 
+const CREATE_USER = gql`
+    mutation CreateUser($input: CreateUserInput!) {
+        createUser(input: $input) {
+            id,
+            name,
+            age
+        }
+    }
+`
+
 function DisplayData() {
-    const {data: userData, loading, error} = useQuery(QUERY_ALL_USERS)
+    const {data: userData, loading, error: userError} = useQuery(QUERY_ALL_USERS)
     const {data: movieData} = useQuery(QUERY_ALL_MOVIES)
 
     // Grab info from the query. [] -> inside comes a func , {} -> what we want to grab from that func
@@ -42,39 +52,68 @@ function DisplayData() {
 
     const [movieSearched, setMovieSearched] = useState("");
 
-    // if (userData) console.log(userData)
     if (loading) return <Preloader/>;
-    if (error) return `Error while fetching user data: ${error}`;
+    if (userError) return `Error while fetching user data: ${userError}`;
+    if (movieError) console.log(movieError)
 
 
     return (
         <>
-            <h1>User list:</h1>
-            <div style={{fontSize: 10, color: "rebeccapurple"}}>
-                {userData &&
-                    userData.users.map((user) => {
-                        return <h2>Username: {user?.username}</h2>
-                    })}
+            <div>
+                <h1>User list:</h1>
+                <div style={{fontSize: 10, color: "rebeccapurple", paddingBottom: "40px"}}>
+                    {userData &&
+                        userData.users.map((user) => {
+                            return <h2
+                                style={{border: "1px solid black", padding: "5px"}}>Username: {user?.username}
+                            </h2>
+                        })}
+                </div>
             </div>
 
-            <div>
-                <input
-                    type="text"
-                    placeholder="Name..."
-                    onChange={(event) => {
-                        console.log(event.target.value)
-                        setMovieSearched(event.target.value);
-                    }}
-                />
+            <div style={{paddingTop: "20px"}}>
 
-                {/*OnClick will call the fetchMovie func from useLazyQuery hook*/}
-                <button onClick={() => {
-                    fetchMovie({variables: {
-                            title: movieSearched,
-                        }
-                    })
+                <h1> Available movies:
+                    {movieData ?
+                        movieData.movies.map((movie) => {
+                            return <h5 style={{fontSize: "15px", padding: "1rem"}}>{movie.title} </h5>
+                        }) : "Nothing here"}
+                </h1>
+
+                <div style={{
+                    display: "flex",
+                    justifyContent: "left",
+                    flexFlow: "column",
+                    width: "200px",
+                    gap: "1rem"
                 }}>
-                    Fetch Movie Data</button>
+                    <div style={{border: "1px solid black", width: "100vw"}}>
+                        <input
+                            type="text"
+                            placeholder="Movie Title..."
+                            onChange={(event) => {
+                                console.log(event.target.value)
+                                setMovieSearched(event.target.value);
+                            }}
+                            style={{
+                                border: "none",
+                                width: "100vw",
+                                borderStyle: "none"
+                            }}
+                        />
+                    </div>
+
+                    {/*OnClick will call the fetchMovie func from useLazyQuery hook*/}
+                    <button onClick={() => {
+                        fetchMovie({
+                            variables: {
+                                title: movieSearched,
+                            }
+                        })
+                    }}>
+                        Fetch Movie Data
+                    </button>
+                </div>
                 <br/>
 
                 <div>
@@ -82,7 +121,11 @@ function DisplayData() {
                         <div>
                             <h1>Movie: {movieSearchedData.movie.title}</h1>
                             <h1>Year: {movieSearchedData.movie.year}</h1>
-                        </div>}
+                        </div>
+                    }
+
+                    {movieError &&
+                        <h1 style={{color: movieError ? "red" : "black"}}>There was an error while fetching </h1>}
                 </div>
             </div>
         </>
